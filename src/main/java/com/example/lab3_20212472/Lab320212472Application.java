6cb75cbabf3b83d53759c6da1d4ce1de6b85abdc
MrFoxito-hack <miguelalvizuri7@gmail.com>
2025-04-25 20:37:36 -0500

    wa

---
+++ src/main/java/com/example/lab3_20212472/Lab320212472Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20212472;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320212472Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320212472Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20212472/Lab320212472ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20212472;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320212472ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
