2e4c47511f8f7dd314084def51630d73ae9401b2
MrFoxito-hack <miguelalvizuri7@gmail.com>
2025-05-11 14:55:00 -0500

    ya limpiando el proyecto, to ok

--- src/main/java/com/example/lab3_20212472/Lab320212472Application.java
+++ src/main/java/com/example/lab3_20212472/Lab320212472Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.example.lab3_20212472.entity")  // Escanea entidades
-@EnableJpaRepositories("com.example.lab3_20212472.repository")  // Escanea repositorios
+@EntityScan("com.example.lab3_20212472.entity")
+@EnableJpaRepositories("com.example.lab3_20212472.repository")
 public class Lab320212472Application {
     public static void main(String[] args) {
         SpringApplication.run(Lab320212472Application.class, args);
